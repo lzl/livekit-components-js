7485f3d2c5bb2766041e86f8bd8136ba65945350
lizunlong <lizunlong@gmail.com>
2023-05-11 18:58:11 +0800

    push.sh

--- packages/styles/push.sh
+++ packages/styles/push.sh
@@ -4,6 +4,6 @@ VERSION=$(cat package.json | grep version | head -1 | awk -F: '{print $2}' | sed
 LAST_NUM=$(echo "${VERSION: -1}")
 UPDATED_LAST_NUM=$((LAST_NUM+1))
 UPDATED_VERSION=${VERSION:0:${#VERSION}-1}$UPDATED_LAST_NUM
-sed -i -e "s/$VERSION/$UPDATED_VERSION/g" package.json
+sed -i '' -e "s/$VERSION/$UPDATED_VERSION/g" package.json
 yalc push
 echo "pushed version $UPDATED_VERSION"
